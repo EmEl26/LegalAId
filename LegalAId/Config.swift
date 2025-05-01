e6100412837c3b9ca610c692dff7d07492e9f7a3
EmEl26 <emily.r.eljamal@gmail.com>
2025-05-01 12:28:54 -0400

    updated to V2 repository

---
+++ LegalAId/Config.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct Config {
+    static let anthropicApiKey = "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"
+}
